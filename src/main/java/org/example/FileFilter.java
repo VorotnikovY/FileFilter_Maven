7ca0a00ec9f7035fb638609e981d6480634a940d
Yuri Vorotnikov <matashandu@gmail.com>
2025-08-16 16:43:59 +0700

    init method was created

--- src/main/java/org/example/FileFilter.java
+++ src/main/java/org/example/FileFilter.java
@@ -5,19 +5,27 @@ import java.io.*;
 public class FileFilter {
 
     public static void main(String[] args) {
-
-        Config config = null;
         try {
-            config = ConfigReader.readConfig(args);
-        } catch (IllegalParameterException e)    {
+            init(args);
+        } catch (IllegalParameterException e) {
             System.out.println(e.getMessage());
             System.exit(1);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Unexpected error: " + e.getMessage());
+            e.printStackTrace();
         }
+    }
+
+    public static void init(String[] args) throws Exception {
+        Config config = ConfigReader.readConfig(args);
 
         try (NumberHandler integerHandler = new NumberHandler(DataType.INTEGER, config);
              NumberHandler floatHandler = new NumberHandler(DataType.FLOAT, config);
              StringHandler stringHandler = new StringHandler(DataType.STRING, config)) {
-
 
             for (String fileName : config.getFileNames()) {
                 try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
@@ -32,12 +40,9 @@ public class FileFilter {
                             stringHandler.process(line);
                         }
                     }
-                } catch (FileNotFoundException e) {
-                    System.out.println("File not found: " + e.getMessage());
-                } catch (IOException e) {
-                    System.out.println("IOException: " + e.getMessage());
                 }
             }
+
             integerHandler.printNumberStatistic();
             floatHandler.printNumberStatistic();
             stringHandler.printStringStatistic();
--- src/test/java/org/example/ConfigReaderTest.java
+++ src/test/java/org/example/ConfigReaderTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ConfigReaderTest {
-    private Throwable thrown;
 
     @Test
     public void correctParamsTest() {
@@ -23,56 +22,56 @@ class ConfigReaderTest {
     @Test
     public void noPrefixProvided()    {
         String[] args = {"-f", "-a", "-p", "in1.txt", "in2.txt"};
-        thrown = assertThrows(IllegalParameterException.class, () -> ConfigReader.readConfig(args), ConfigReader.NO_PREFIX_ERROR_MESSAGE);
+        Throwable thrown = assertThrows(IllegalParameterException.class, () -> ConfigReader.readConfig(args), ConfigReader.NO_PREFIX_ERROR_MESSAGE);
         assertEquals(ConfigReader.NO_PREFIX_ERROR_MESSAGE, thrown.getMessage());
     }
 
     @Test
     public void noPathProvided()    {
         String[] args = {"-f", "-a", "-o", "-p", "sample-", "in1.txt", "in2.txt"};
-        thrown = assertThrows(IllegalParameterException.class, () -> ConfigReader.readConfig(args));
+        Throwable thrown = assertThrows(IllegalParameterException.class, () -> ConfigReader.readConfig(args));
         assertEquals(ConfigReader.NO_NEW_PATH_ERROR_MESSAGE, thrown.getMessage());
     }
 
     @Test
     public void multipleStatParams()    {
         String[] args = {"-f", "-a", "-s", "-p", "sample-", "in1.txt", "in2.txt"};
-        thrown = assertThrows(IllegalParameterException.class, () -> ConfigReader.readConfig(args));
+        Throwable thrown = assertThrows(IllegalParameterException.class, () -> ConfigReader.readConfig(args));
         assertEquals(ConfigReader.MULTIPLE_STATISTIC_PARAMETERS_ERROR_MESSAGE, thrown.getMessage());
     }
 
     @Test
     public void multipleAppendParams()    {
         String[] args = {"-f", "-a", "-a", "-p", "sample-", "in1.txt", "in2.txt"};
-        thrown = assertThrows(IllegalParameterException.class, () -> ConfigReader.readConfig(args));
+        Throwable thrown = assertThrows(IllegalParameterException.class, () -> ConfigReader.readConfig(args));
         assertEquals(ConfigReader.MULTIPLE_APPEND_ERROR_MESSAGE, thrown.getMessage());
     }
 
     @Test
     public void multiplePathParams()    {
         String[] args = {"-f", "-a", "-o", "D:/Work", "-o", "-p", "sample-", "in1.txt", "in2.txt"};
-        thrown = assertThrows(IllegalParameterException.class, () -> ConfigReader.readConfig(args));
+        Throwable thrown = assertThrows(IllegalParameterException.class, () -> ConfigReader.readConfig(args));
         assertEquals(ConfigReader.MULTIPLE_NEW_PATH_ERROR_MESSAGE, thrown.getMessage());
     }
 
     @Test
     public void multiplePrefixParams()    {
         String[] args = {"-f", "-a", "-p", "sample-", "-p", "in1.txt", "in2.txt"};
-        thrown = assertThrows(IllegalParameterException.class, () -> ConfigReader.readConfig(args));
+        Throwable thrown = assertThrows(IllegalParameterException.class, () -> ConfigReader.readConfig(args));
         assertEquals(ConfigReader.MULTIPLE_PREFIX_ERROR_MESSAGE, thrown.getMessage());
     }
 
     @Test
     public void noParams()    {
         String[] args = new String[0];
-        thrown = assertThrows(IllegalParameterException.class, () -> ConfigReader.readConfig(args));
+        Throwable thrown = assertThrows(IllegalParameterException.class, () -> ConfigReader.readConfig(args));
         assertEquals(ConfigReader.NO_PARAMETERS_ERROR_MESSAGE, thrown.getMessage());
     }
 
     @Test
     public void noFiles()    {
         String[] args = {"-f", "-a", "-p", "sample-", "-o", "D:/Work"};
-        thrown = assertThrows(IllegalParameterException.class, () -> ConfigReader.readConfig(args));
+        Throwable thrown = assertThrows(IllegalParameterException.class, () -> ConfigReader.readConfig(args));
         assertEquals(ConfigReader.NO_FILENAMES_ERROR_MESSAGE, thrown.getMessage());
     }
 }
